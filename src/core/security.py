71632b9b86876f94f746d60e6d999a360a094695
Daminin <nicksalcedo717@gmail.com>
2025-11-30 12:08:11 -0500

    feat: extend JWT access token expiration to 30 days and add initial Dockerfile and .dockerignore

--- src/core/security.py
+++ src/core/security.py
@@ -39,7 +39,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None): 
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(days=30)
     to_encode.update({"exp": expire}) # Añade tiempo de expiración
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
